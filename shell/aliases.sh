7707d079e67c00f400ad833a523fc57789358db6
Odinei Ramone <odineiramone@gmail.com>
2019-02-07 22:13:55 -0200

    add postgres commands

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,10 +1,18 @@
 alias be='bundle exec'
 alias atom='open -a atom'
 alias bash_profile='atom ~/.bash_profile'
+alias source_bash_profile='source ~/.bash_profile'
+
+# Postgres installed by brew
+
 alias postgre_start='brew services start postgresql'
 alias postgre_stop='brew services stop postgresql'
 alias postgre_restart='brew services restart postgresql'
-alias source_bash_profile='source ~/.bash_profile'
+
+# Postgres installed by asdf
+
+alias pg_start='pg_ctl start'
+alias pg_stop='pg_ctl stop'
 
 alias go_go_shoryuken='bundle exec shoryuken -R -C config/shoryuken.yml'
 alias jupyter_notebook='jupyter notebook'
